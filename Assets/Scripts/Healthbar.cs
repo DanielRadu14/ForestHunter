5ec8c9c4605e75ed3bdf7ee29008b355e606a668
DanielRadu14 <danielradu148@gmail.com>
2023-12-06 16:55:24 +0200

    finalized health bar for opponents

--- Assets/Scripts/Healthbar.cs
+++ Assets/Scripts/Healthbar.cs
@@ -6,9 +6,23 @@ using UnityEngine.UI;
 public class Healthbar : MonoBehaviour
 {
     [SerializeField] private Image healthbarSprite;
+    [SerializeField] private float reduceSpeed = 2;
+    private float target = 1;
+    private Camera camera;
+
+    private void Start()
+    {
+        camera = Camera.main;
+    }
 
     public void UpdateHealthBar(float maxHealth, float currentHealth)
     {
-        healthbarSprite.fillAmount = currentHealth / maxHealth;
+        target = currentHealth / maxHealth;
+    }
+
+    private void Update()
+    {
+        transform.rotation = Quaternion.LookRotation(transform.position - camera.transform.position);
+        healthbarSprite.fillAmount = Mathf.MoveTowards(healthbarSprite.fillAmount, target, reduceSpeed * Time.deltaTime);
     }
 }
